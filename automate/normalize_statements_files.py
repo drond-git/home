58d25a552d4d53460f0349d7ca917bb269b41296
V Sh <vshipitsyn@gmail.com>
2013-10-26 03:09:39 -0700

    Match on more formats. Refactored regexp construction.

--- automate/normalize_statements_files.py
+++ automate/normalize_statements_files.py
@@ -16,6 +16,12 @@ STATEMENTS_ROOT_DIR = (
     'statements')
 
 
+RE_YYYY = r'(?P<yyyy>20[0-2]\d)'  # 2000-2029
+RE_MM = r'(?P<mm>0\d|1[0-2])'  # 00-12
+RE_DD = r'(?P<dd>[012]\d|3[01])'  # 00-31
+RE_EXT = r'(?P<ext>\..+)'  # .pdf
+
+
 def GenerateMmByMonthMap():
   mm_by_month_map = {}
   for i, month in enumerate((
@@ -23,6 +29,7 @@ def GenerateMmByMonthMap():
        'july', 'august', 'september', 'october', 'november', 'december')):
       mm_by_month_map[month] = '%02d' % (i + 1)
   return mm_by_month_map
+
 
 MM_BY_MONTH_MAP = GenerateMmByMonthMap()
 
@@ -36,29 +43,44 @@ def ListFilesRecursively(directory):
         yield dirpath, filename
 
 
+def MakeKeywords(input):
+  """Returns the input dict with select key values adjusted."""
+  if 'ext' in input:
+    input['ext'] = input['ext'].lower()
+  if 'dd' not in input:
+    input['dd'] = '00'
+  return input
+
+
 def TryNormalize(dirpath, filename, dry_run=True):
   """If necessary, normalized the file. Returns a tuple."""
   logging.debug('Trying to normalize %r in %r.', filename, dirpath)
   # First try the cases where reordering extracted date values is enough.
   simple_conversions = [
+    # 20120930.pdf
+    r'^%s%s%s%s$' % (RE_YYYY, RE_MM, RE_DD, RE_EXT),
+    # 09302012.pdf
+    r'^%s%s%s%s$' % (RE_DD, RE_MM, RE_YYYY, RE_EXT),
+    # 2012_09.pdf
+    r'^%s_%s%s$' % (RE_YYYY, RE_MM, RE_EXT),
     # eStmt_2012-09-07.pdf
-    r'^eStmt_(?P<yyyy>\d\d\d\d)-(?P<mm>\d\d)-(?P<dd>\d\d)(?P<ext>\..+)$',
+    r'^eStmt_%s-%s-%s%s$' % (RE_YYYY, RE_MM, RE_DD, RE_EXT),
   ]
   for regexp in simple_conversions:
     match = re.search(regexp, filename)
     if match:
-      return '%(yyyy)s%(mm)s%(dd)s-eStmt%(ext)s)' % match.groupdict()
+      return '%(yyyy)s%(mm)s%(dd)s%(ext)s)' % (
+          MakeKeywords(match.groupdict()))
   # Then try when custom computation is needed.
   # November2012_7257.qfx
   match = re.search(
-      r'(?P<month>[a-zA-Z]+)(?P<yyyy>\d{4})_(?P<four>\d{4})(?P<ext>\.(?:qfx|QFX))',
+      r'(?P<month>[a-zA-Z]+)%s_(?P<four>\d{4})%s' % (RE_YYYY, RE_EXT),
       filename)
   if match:
-    keywords = match.groupdict()
+    keywords = MakeKeywords(match.groupdict())
     keywords['mm'] = MM_BY_MONTH_MAP[keywords['month'].lower()]
-    return '%(yyyy)s%(mm)s01%(ext)s)' % keywords
-  
-  return None  # None if not matched any renamer expression..
+    return '%(yyyy)s%(mm)s00%(ext)s)' % keywords
+  return None  # None if not matched any renamer expression.
 
 
 def NormalizeStatementsFiles():
